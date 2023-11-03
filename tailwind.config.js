b959d4c1619774a7d4cd28961c93bbabfc4efb29
UyenPun <tranuyen2018@gmail.com>
2023-11-03 17:36:09 +0700

    Ad lifestyle-stories.

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,11 +19,9 @@ module.exports = {
           "0%": { transform: "translateY(-100%)" },
           "100%": { transform: "translateY(0%)" },
         },
-        fadeIn: { from: { opacity: 0 }, to: { opacity: 1 } },
       },
       animation: {
         slideDown: "slideDown 0.4s ease-in-out",
-        fadeIn: "fadeIn .5s ease-in-out",
       },
       backgroundImage: {
         "slider-bg": 'url("./img/slider-bg.jpeg")',
